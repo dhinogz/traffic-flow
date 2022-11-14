983f4bc91d6b79f7f11b201b221f4bbd617b9db2
dhinogz <davidhinojosagzz@gmail.com>
2022-11-14 15:13:13 -0600

    Initial commit

--- simulation/traffic_flow/api/router.py
+++ simulation/traffic_flow/api/router.py
@@ -5,5 +5,6 @@ from api.flock.views import router as flock_router
 
 api_router = APIRouter()
 api_router.include_router(system_router, prefix="/system", tags=["system"])
+api_router.include_router(virus_router, prefix="/traffic", tags=["traffic-flow"])
 api_router.include_router(virus_router, prefix="/virus", tags=["virus-model"])
 api_router.include_router(flock_router, prefix="/flock", tags=["flock"])
