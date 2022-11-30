69ce7a169d13fec8f91f31f696605c4332f33680
dhinogz <davidhinojosagzz@gmail.com>
2022-11-30 16:08:31 -0600

    Add test endpoint

--- simulation/traffic_flow/api/traffic_flow/services.py
+++ simulation/traffic_flow/api/traffic_flow/services.py
@@ -18,9 +18,23 @@ def parse_model_results(results) -> list[CarRead]:
 def run_model(traffic_params: TrafficParams) -> list[CarRead]:
     from models.traffic_flow import TrafficFlowModel
 
-    model = TrafficFlowModel(traffic_params)
+    constant_parameters = {
+        'sizeX': 100, #Constante que corresponde al tamaño en del borde x
+        'sizeY': 10, #Constante que corresponde al tamaño en del borde y
+        'ndim': 2, #Constante que corresponde a la dimension de la simulacion
+        'population': 1, #Constante que corresponde a la poblacion inicial de la autopista
+        'populationIncor': 1, #Constante que corresponde a la poblacion inicial de la incorporacion
+        'cars_pos':[np.array([0, 5.])], #Constante que corresponde a la posicion inicial de la autopista
+        'cars_posIncor':[np.array([20, 2.])], #Constante que corresponde a la posicion inicial de la incorporacion
+    }
+
+    parameters = {**traffic_params.dict(), **constant_parameters}
+
+    model = TrafficFlowModel(parameters)
 
     res = model.run()
+
+    # pandas.concat([results["variables"]["Car"], results["variables"]["CarIncor"]]).sort_values(by=['id', "t"])
 
     return parse_model_results(res)
 
--- simulation/traffic_flow/api/traffic_flow/views.py
+++ simulation/traffic_flow/api/traffic_flow/views.py
@@ -5,6 +5,90 @@ from .services import run_model
 
 router = APIRouter()
 
+
+@router.get(
+    "info",
+    response_model=list[CarRead],
+)
+async def get_traffic_info() -> list[CarRead]:
+
+    return [
+        {
+            "id": 1,
+            "positions": [
+                {
+                    "step": 1,
+                    "pos_x": 2,
+                    "pos_y": 2,
+                },
+                {
+                    "step": 2,
+                    "pos_x": 3,
+                    "pos_y": 2,
+                },
+            ]
+        },
+        {
+            "id": 2,
+            "positions": [
+                {
+                    "step": 2,
+                    "pos_x": 2,
+                    "pos_y": 2,
+                },
+                {
+                    "step": 3,
+                    "pos_x": 3,
+                    "pos_y": 2,
+                },
+            ]
+        },
+    ]
+
+    return [
+            {
+                "step": 1,
+                "cars": [    
+                    {
+                        "id": 1,
+                        "pos_x": 2,
+                        "pos_y": 2,
+                    },
+                    {
+                        "id": 2,
+                        "pos_x": 4,
+                        "pos_y": 2,
+                    },
+                    {
+                        "id": 3,
+                        "pos_x": 5,
+                        "pos_y": 2,
+                    },
+                ],
+            },
+            {
+                "step": 2,
+                "cars": [    
+                    {
+                        "id": 1,
+                        "pos_x": 2,
+                        "pos_y": 2,
+                    },
+                    {
+                        "id": 2,
+                        "pos_x": 5,
+                        "pos_y": 2,
+                    },
+                    {
+                        "id": 3,
+                        "pos_x": 6,
+                        "pos_y": 2,
+                    },
+                ]
+            }
+        ]
+
+
 @router.post(
     "", 
     response_model=list[CarRead]
